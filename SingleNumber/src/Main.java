22da3bcc8d5cc8f0d6807f461e598705dec0b4a0
nguyenvanquang97 <quangdtkc@gmail.com>
2022-08-26 02:23:43 +0700

    1

--- SingleNumber/src/Main.java
+++ SingleNumber/src/Main.java
@@ -3,11 +3,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Main {
-    public static void main(String[] args) {
-        int [] arr={4,1,2,1,2};
-        int a=singleNumber(arr);
-        System.out.println(a);
-    }
     public static int singleNumber(int[] nums) {
         HashSet<Integer> hashSet=new HashSet<>();
         ArrayList<Integer> clone=new ArrayList<>();
@@ -18,7 +13,6 @@ public class Main {
         ArrayList<Integer> integers=new ArrayList<>();
        integers.addAll(hashSet);
        integers.addAll(hashSet);
-       System.out.println();
        for (Integer i:clone){
            integers.remove(i);
        }
