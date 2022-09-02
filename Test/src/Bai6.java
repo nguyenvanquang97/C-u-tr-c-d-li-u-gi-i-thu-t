73a7a773fdfe35d229854a691300bade2ab45d26
nguyenvanquang97 <quangdtkc@gmail.com>
2022-09-02 15:16:05 +0700

    1

---
+++ Test/src/Bai6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Bai6 {
+
+    public static boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Stack<Integer> stack = new Stack();
+        boolean[] seen = new boolean[rooms.size()];
+        seen[0] = true;
+        stack.push(0);
+        while (!stack.isEmpty()) {
+            int key = stack.pop();
+            for (int newKey : rooms.get(key)) {
+                if (!seen[newKey]) {
+                    seen[newKey] = true;
+                    stack.push(newKey);
+                }
+            }
+        }
+        for (boolean room: seen) {
+            if (room == false) return false;
+        }
+        return true;
+    }
+}
